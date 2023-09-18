369565bbbbed1779b26587c44f98de65eef7e55c
David Kneringer Foss <david.k.foss@gmail.com>
2023-09-18 11:35:43 +0200

    Shows the shortest path as an animation

--- a_star.py
+++ a_star.py
@@ -1,10 +1,22 @@
+from PIL import Image
+import pygame
+import time
+import pandas as pd
 from Map import Map_Obj
 from math import sqrt
 import numpy as np
 np.set_printoptions(threshold=np.inf, linewidth=300)
-import pandas as pd
-import time
-from PIL import Image
+
+YELLOW = (20, 204, 20)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (166, 166, 166)
+GREEN = (0, 204, 0)
+ORANGE = (255, 102, 0)
+BLUE = (0, 128, 255)
+colors = {-1: (255, 0, 0), 1: (215, 215, 215),
+          2: (166, 166, 166), 3: (96, 96, 96), 4: (36, 36, 36)}
+
 
 class Node:
     def __init__(self, x, y, parent=None):
@@ -14,24 +26,29 @@ class Node:
         self.g = 0
         self.h = 0
         self.explored = False
-        
+
     def f(self):
         return self.get_g() + self.get_h()
+
     def get_g(self):
         return self.g
+
     def get_h(self):
         return self.h
+
     def set_h(self, h):
         self.h = h
+
     def set_g(self, g):
-        self.g = g 
+        self.g = g
+
 
 def search(maze, start_pos, goal_pos):
     opened_nodes = []
-    closed_nodes = []   
+    closed_nodes = []
     opened_nodes.append(Node(start_pos[0], start_pos[1]))
     no_rows, no_columns = np.shape(maze)
-    
+
     for i in range(1000):
         current_node = opened_nodes[0]
         current_index = 0
@@ -52,7 +69,7 @@ def search(maze, start_pos, goal_pos):
                     continue
                 neighbor_x = current_node.x + i
                 neighbor_y = current_node.y + j
-                if (neighbor_x > (no_rows - 1) or neighbor_x < 0 or neighbor_y > (no_columns -1) or neighbor_y < 0):
+                if (neighbor_x > (no_rows - 1) or neighbor_x < 0 or neighbor_y > (no_columns - 1) or neighbor_y < 0):
                     continue
                 if maze[neighbor_x][neighbor_y] == -1:
                     continue
@@ -67,53 +84,74 @@ def search(maze, start_pos, goal_pos):
                 if node.x == closed_node.x and node.y == closed_node.y:
                     visited = True
             if visited:
-                continue              
+                continue
             node.set_g(current_node.get_g() + maze[node.x][node.y])
-            node.set_h(abs(neighbor.x - goal_pos[0]) + abs(node.y - goal_pos[1]))
+            node.set_h(
+                abs(neighbor.x - goal_pos[0]) + abs(node.y - goal_pos[1]))
             if len([i for i in opened_nodes if node == i and node.get_g() > i.get_g()]) > 0:
                 continue
             opened_nodes.append(node)
     samf, path = return_path(current_node, maze)
     return opened_nodes, closed_nodes, samf, path
 
-def show_path(map, opened_nodes, closed_nodes, path):
-        width = map.shape[1]
-        height = map.shape[0]
-        scale = 20
-        # Create an all-yellow image
-        image = Image.new('RGB', (width * scale, height * scale), (255, 102, 255))
-        pixels = image.load()
 
-        # Define what colors to give to different values of the string map (undefined values will remain yellow, this is
-        colors = {-1: (255, 0, 0), 1: (215, 215, 215), 2: (166, 166, 166), 3: (96, 96, 96), 4: (36, 36, 36),
-         8: (255, 102, 255) }
-                 
+def show_path(maze, path):
+    # Pygame initialization
+    pygame.init()
+    scale = 20
+    width = maze.shape[1] * scale
+    height = maze.shape[0] * scale
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Pathfinding Visualization")
 
-        """{' # ': (255, 0, 0), ' . ': (215, 215, 215), ' , ': (166, 166, 166), ' : ': (96, 96, 96),
-                  ' ; ': (36, 36, 36), ' S ': (255, 0, 255), ' G ': (0, 128, 255)}"""
-        # Go through image and set pixel color for every position
-        for y in range(height):
-            for x in range(width):
-                if map[y][x] not in colors: continue
-                for i in range(scale):
-                    for j in range(scale):
-                        pixels[x * scale + i, y * scale + j] = colors[map[y][x]]
-        
-        for node in closed_nodes:
-            if (node.x, node.y) in path: continue
-            for i in range(scale):
-                    for j in range(scale):
-                        pixels[node.y * scale + i, node.x * scale + j] = (0, 204, 0)
+    running = True
+    clock = pygame.time.Clock()
+    # Milliseconds delay between each frame (adjust as needed)
+    draw_delay = 100
+    draw_counter = 0
 
-        for node in opened_nodes:
-            if (node.x, node.y) in path: continue
-            color = (255, 102, 0)
-            for i in range(scale):
-                for j in range(scale):
-                    pixels[node.y * scale + i, node.x * scale + j] = color
+    # Create a list to store the frames of the path
+    path_frames = []
 
-        # Show image
-        image.show()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # Clear the screen
+        screen.fill((215, 215, 215))
+
+        # Draw the maze
+        for y in range(maze.shape[0]):
+            for x in range(maze.shape[1]):
+                if maze[y][x] in colors:
+                    pygame.draw.rect(screen, colors[maze[y][x]], (x * scale,
+                                     y * scale, scale, scale))
+
+        # Draw the path in blue
+        if draw_counter < len(path):
+            node_x, node_y = path[draw_counter]
+            pygame.draw.rect(screen, BLUE, (node_y * scale,
+                             node_x * scale, scale, scale))
+            draw_counter += 1
+
+        pygame.display.flip()
+
+        # Create a snapshot of the current frame and add it to the path_frames list
+        path_frames.append(pygame.surfarray.array3d(screen))
+
+        clock.tick(60)
+        pygame.time.delay(draw_delay)
+
+        # If the path is fully drawn, replay the frames
+        if draw_counter >= len(path) and len(path_frames) > 0:
+            for frame in path_frames:
+                screen.blit(pygame.surfarray.make_surface(frame), (0, 0))
+                pygame.display.flip()
+                pygame.time.delay(draw_delay)
+
+    pygame.quit()
+
 
 def return_path(current_node, map_int_np):
     path = []
@@ -124,16 +162,19 @@ def return_path(current_node, map_int_np):
         current = current.parent
 
     path = path[::-1]
-   
+
     for i in range(len(path)):
         result[path[i][0]][path[i][1]] = 8
     return result, path
 
+
 def main():
-    map_obj = Map_Obj(task=4) #Choose task 1, 2, 3 or 4.
+    map_obj = Map_Obj(task=3)  # Choose task 1, 2, 3 or 4.
     map_int_np, map_str_np = map_obj.get_maps()
     start = map_obj.get_start_pos()
     goal = map_obj.get_goal_pos()
     opened_nodes, closed_nodes, maze, path = search(map_int_np, start, goal)
-    show_path(maze, opened_nodes, closed_nodes, path)
+    show_path(maze, path)
+
+
 main()
